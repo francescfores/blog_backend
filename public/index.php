cf1b98a4973ebfe089d121ce0bdd77972d7757e7
ffores <ffores93@gmail.com>
2024-09-25 20:59:29 +0200

    vercel new database aws rds

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 header('Access-Control-Allow-Credentials: true');
-#header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization','*');
 
