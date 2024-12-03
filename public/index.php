be7878e7e6d27bc989233f284b1a93314c043fe6
Francesc Forés <francescfores@users.noreply.github.com>
2024-12-03 18:36:02 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
-header('Access-Control-Allow-Credentials: true');
+//header('Access-Control-Allow-Credentials: true');
 
 define('LARAVEL_START', microtime(true));
 
